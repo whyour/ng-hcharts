be7d72e399be3ea1300799bd6b71fd62e710db03
王鹏 <wangpengwnba@126.com>
2018-05-06 22:14:52 +0800

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { HighchartsChartComponent } from './highcharts-chart.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighchartsChartComponent
   ],
   imports: [
     BrowserModule
